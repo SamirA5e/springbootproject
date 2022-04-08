cc1aca9ac5fcb92e9c296334aa8c37252e6f36f0
SamirA5e <samirs@a5econsulting.net>
2022-04-08 12:06:52 +0530

    Update ProjectRepository.java

--- src/main/java/com/onlyjavatech/samir/repository/ProjectRepository/ProjectRepository.java
+++ src/main/java/com/onlyjavatech/samir/repository/ProjectRepository/ProjectRepository.java
@@ -1,13 +1,9 @@
 package com.onlyjavatech.samir.repository.ProjectRepository;
 
-import com.onlyjavatech.samir.model.Employee;
 import com.onlyjavatech.samir.model.ProjectModel.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, String> {
-//    List<Employee> findByEmployees_id(String id);
 }
